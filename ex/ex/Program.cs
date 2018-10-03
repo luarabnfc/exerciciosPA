df88f0281923f2b80419fba4e2f1e68a8db3da4d
Luara <luarabnfc@gmail.com>
2018-10-03 17:02:21 -0300

    exerciciosPA

---
+++ ex/ex/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício 1");
+            Console.WriteLine("***********");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("OBS: Caso queira encerrar o programa digite 0!!");
+            Console.WriteLine();
+
+            int soma = 0;
+            int i = 0;
+            int num = 1;
+            int idade = 0;
+
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            do
+            {
+                Console.Write("Digite a " + num + "º idade: ");
+                
+                idade = Convert.ToInt32(Console.ReadLine());
+
+                if (idade != 0)
+                {
+                    i++;
+                }
+                num++;
+                soma = soma + idade;
+
+            } while (idade != 0);
+
+            int media = soma / i;
+
+            Console.WriteLine("O Resultado da média das idades é: " + media);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ex2/ex2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício 2");
+            Console.WriteLine("***********");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("OBS: Caso queira encerrar o prrograma digite -1!!");
+            Console.WriteLine();
+
+            int i = 0;
+            int num = 1;
+            double soma = 0;
+            double nota = 0;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            do
+            {
+                Console.Write("Digite a " + num + "º nota: ");
+                nota = Convert.ToDouble(Console.ReadLine());
+                if( nota != -1)
+                {
+                    i++;
+                    soma = soma + nota;
+                }
+                
+                num++;
+               
+
+            }while(nota != -1);
+            
+            double media = soma / i;
+
+            Console.WriteLine("A média das notas é: " + media);
+            Console.ReadKey();
+        }
+    }
+}
